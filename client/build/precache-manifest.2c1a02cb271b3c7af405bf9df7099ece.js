78d5858717a3cfb353190ca608c414b49f4a1424
AndreasIrta <andreasirta0711@gmail.com>
2021-07-31 00:19:37 -0600

    finalize everything

---
+++ client/build/precache-manifest.2c1a02cb271b3c7af405bf9df7099ece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaed07514f347c7cf803f16d5838fe82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d6da6546a5f24fb5e3",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "2e3c6efd5d3f42033c58",
+    "url": "/static/js/2.7c35570f.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.7c35570f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d6da6546a5f24fb5e3",
+    "url": "/static/js/main.51302d34.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
